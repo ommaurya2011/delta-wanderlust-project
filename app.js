b5ec1a17088c7bb36c8b1e409caea72b59f083e0
Om Maurya <ommauryaom2011@gmail.com>
2025-11-13 18:49:16 +0530

    updated2

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const userRouter =  require("./routes/user.js");
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-// const dbUrl = process.env.ATLASDB_URL;
-const dbUrl = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+// const dbUrl = "mongodb://127.0.0.1:27017/wanderlust";
 
 main().then(() => {
     console.log("connected to DB");
